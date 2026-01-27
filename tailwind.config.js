8de792291db69eb8ad4836326ed64aa1f71bf399
Luqman Herifa <luqmanherifa@gmail.com>
2026-01-27 17:17:35 +0700

    Add Firebase, fonts, and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        softOlive: "#606C38", // Young Olive Green
+        deepOlive: "#283618", // Dark Earthy Green
+        warmCream: "#FEFAE0", // Bright Warm Off-white
+        caramelTan: "#DDA15E", // Light Warm Brown
+        rusticBrown: "#BC6C25", // Deep Warm Brown
+      },
+      fontFamily: {
+        heading: ["Archivo", "sans-serif"],
+        body: ["Questrial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
